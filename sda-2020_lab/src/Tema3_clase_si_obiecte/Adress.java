6cd8b9d1d1fb8091bc6d4dfc2d311ff581135175
marina1198 <comanmarina846@gmail.com>
2020-03-31 18:06:34 +0300

    Teme

---
+++ sda-2020_lab/src/Tema3_clase_si_obiecte/Adress.java
@@ -0,0 +1,33 @@
+package Tema3_clase_si_obiecte;
+
+public class Adress {
+
+	private String street;
+	private String city;
+	private int number;
+
+	public String getStreet() {
+		return street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public Adress(String street, String city, int number) {
+		super();
+		this.street = street;
+		this.city = city;
+		this.number = number;
+	}
+
+	public String getFullAdress() {
+
+		return city + street + number;
+	}
+
+}
---
+++ sda-2020_lab/src/Tema3_clase_si_obiecte/PaymentMethod.java
@@ -0,0 +1,16 @@
+package Tema3_clase_si_obiecte;
+
+public class PaymentMethod {
+
+	private String type;
+
+	public String getType() {
+		return type;
+	}
+
+	public PaymentMethod(String type) {
+		super();
+		this.type = type;
+	}
+
+}
